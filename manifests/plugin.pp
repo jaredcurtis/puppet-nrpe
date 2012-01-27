706234570d17a154d62fb9feb22b4347dd64a549
Jared Curtis <jcurtis@ncircle.com>
2012-01-26 23:23:24 -0800

    Adding nrpe::plugin to manage installing NRPE plugins

---
+++ manifests/plugin.pp
@@ -0,0 +1,15 @@
+define nrpe::plugin(
+  $plugin=$name,
+  $plugindir=$nrpe::params::plugindir,
+  $source="puppet:///modules/${module_name}/plugins/${plugin}"
+) {
+  include nrpe::params
+
+  file { "${plugindir}/${plugin}":
+    source  => $source,
+    owner   => $nrpe::params::user,
+    group   => $nrpe::params::group,
+    mode    => '0744',
+    notify  => Service['nrpe'];
+  }
+}
